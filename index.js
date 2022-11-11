5cf6455808a4ac342629e199ad52b1440bc2b510
Nzy <nidal.chowdhury1@gmail.com>
2022-11-12 10:20:16 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+let isModalOpen = false;
+let contrastToggle = false
+const scaleFactor = 1 / 20
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape")
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+  
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0
+    const booleanInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * booleanInt}px, ${y * booleanInt}px)`
+  }
+}
+
+function toggelContrast() {
+  contrastToggle = !contrastToggle
+  if (contrastToggle) {
+    document.body.classList += " dark-theme"
+  } else {
+    document.body.classList.remove("dark-theme")
+  }
+}
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      'service_ninx7lp',
+      'template_fpp8gjk',
+      event.target,
+      'lPLZsiOp8f-EMujJl'
+    ).then(() => {
+      loading.classList.remove("modal__overlay--visible")
+      success.classList += " modal__overlay--visible"
+    }).catch(() => {
+      loading.classList.remove("modal__overlay--visible")
+      alert(
+        "The email service is temporary unavailable. Please contact me directly on nidal.chowdhury1@gmail.com"
+      )
+    })
+}
+
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false
+    return document.body.classList.remove("modal--open")
+  }
+  isModalOpen = true
+  document.body.classList += " modal--open"
+}
